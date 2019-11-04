0b011543876ccacc2c365dba0012308aa0ae2568
Qureshi63 <mnqnoorq@gmail.com>
2019-11-05 00:51:30 +0500

    first

---
+++ main.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+    Login
+    </title>
+    <link href="main.css" rel="stylesheet">
+<head>
+<body class="main">
+    <div class="box">
+        <h1>
+            University Login
+        <h1>
+        <h2>
+            Login Type
+        </h2>
+            <p align="center">
+            <input class="b1" type="submit" name="teacher_login" value="Teacher Login" 
+            onclick="window.location.href='http://localhost:8080/assignment_project/teacher_login.php'">
+        <br>
+            <input class="b1" type="submit" name="student_login" value="Student Login" 
+            onclick="window.location.href='http://localhost:8080/assignment_project/student_login.php'">
+            </p>
+    </div>
+</body>
+<html>
